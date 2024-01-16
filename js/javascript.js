2e9ab8408671e4d8011329a5032eadb076d07ba6
waras47 <saudinwahyudin@gmail.com>
2024-01-16 21:16:24 +0700

    frist commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,17 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks =  document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const navbar = document.getElementsByClassName('navbar')[0];
 
+// Tambahkan event listener untuk mendeteksi event scroll pada window
+window.addEventListener('scroll', () => {
+    // Jika posisi scroll lebih dari 0 (scroll ke bawah), tambahkan class 'scrolled' pada navbar, selain itu, hapus class 'scrolled'
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+    navbarLinks.classList.toggle('active');
+});
